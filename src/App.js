92793d29e2374a4da470ceaa3f002d497bc6c981
ashychiu <ashleychiu.codes@gmail.com>
2023-01-25 16:42:19 -0800

    Add styling with MUI

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import { useState } from "react";
 import "./App.css";
+import { Button, Grid } from "@mui/material";
 
 function App() {
-  const [buttonColor, setButtonColor] = useState("red")
-  const newButtonColor = buttonColor === "red" ? "blue" : "red"
+  const [buttonColor, setButtonColor] = useState("red");
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
 
   const changeColor = () => {
-    setButtonColor(newButtonColor)
-  }
+    setButtonColor(newButtonColor);
+  };
   return (
-    <div className="App">
-      <button onClick={changeColor} style={{ backgroundColor: buttonColor }}>Change to {newButtonColor}</button>
-      <input type="checkbox" />
-    </div>
+    <Grid
+      container
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Grid item>
+        <Button
+          onClick={changeColor}
+          style={{ backgroundColor: buttonColor }}
+          variant="contained"
+        >
+          Change to {newButtonColor}
+        </Button>
+        <input type="checkbox" />
+      </Grid>
+    </Grid>
   );
 }
 
